6ccde5d2215b20df14f412e26b605d31bcb2520f
Jeffery Abbott <jeff@jefferyabbott.com>
2021-07-15 15:46:50 -0700

    Setup router

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,9 @@
 import express from 'express';
+import routes from './routes/index';
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-  console.log('connected!');
-  return res.send('We are now connected AGAIN');
-});
+app.use('/api', routes);
 
 app.listen(port, () => {
   console.log(`Server started on port ${port}.`);
