775712604c68a0fbe197cd69dc1078fc5c03bda5
Jeffery Abbott <jeff@jefferyabbott.com>
2021-07-15 15:42:49 -0700

    Fix Typescript build duplication

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,12 @@
-console.log('This is a test');
+import express from 'express';
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  console.log('connected!');
+  return res.send('We are now connected AGAIN');
+});
+
+app.listen(port, () => {
+  console.log(`Server started on port ${port}.`);
+});
