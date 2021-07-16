93a14f3d35cd1ae8e07e5021b40d60acd5b99a57
Jeffery Abbott <jeff@jefferyabbott.com>
2021-07-15 19:32:46 -0700

    update port assignment

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,7 +2,6 @@ import express from 'express';
 const routes = express.Router();
 
 routes.get('/', (req, res) => {
-  console.log('connected!');
   return res.send('main api route');
 });
 
--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,7 @@
 import express from 'express';
 import routes from './routes/index';
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use('/api', routes);
 
