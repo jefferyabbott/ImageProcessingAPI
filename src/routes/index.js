0196eb5452f2ec50e7be7ea1fcb9ea98f1470e5e
Jeffery Abbott <jeff@jefferyabbott.com>
2021-07-20 09:55:59 -0700

    Add nodemon and ts-node

---
+++ src/routes/index.js
@@ -0,0 +1,90 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __generator = (this && this.__generator) || function (thisArg, body) {
+    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
+    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
+    function verb(n) { return function (v) { return step([n, v]); }; }
+    function step(op) {
+        if (f) throw new TypeError("Generator is already executing.");
+        while (_) try {
+            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
+            if (y = 0, t) op = [op[0] & 2, t.value];
+            switch (op[0]) {
+                case 0: case 1: t = op; break;
+                case 4: _.label++; return { value: op[1], done: false };
+                case 5: _.label++; y = op[1]; op = [0]; continue;
+                case 7: op = _.ops.pop(); _.trys.pop(); continue;
+                default:
+                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
+                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
+                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
+                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
+                    if (t[2]) _.ops.pop();
+                    _.trys.pop(); continue;
+            }
+            op = body.call(thisArg, _);
+        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
+        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
+    }
+};
+exports.__esModule = true;
+var express_1 = require("express");
+var routes = express_1["default"].Router();
+var path = require('path');
+var fileOperations_1 = require("../services/fileOperations");
+var queryChecker_1 = require("../services/queryChecker");
+var fullImage = 'full';
+var thumbnailImage = 'thumb';
+routes.get('/images', function (req, res) { return __awaiter(void 0, void 0, void 0, function () {
+    var filename, heightInput, widthInput, incompleteQuery, height, width, thumbnailFilename, thumbnailFileExtension, targetFilename, imageResized;
+    return __generator(this, function (_a) {
+        switch (_a.label) {
+            case 0:
+                filename = req.query.filename;
+                heightInput = req.query.height;
+                widthInput = req.query.width;
+                incompleteQuery = queryChecker_1.validateQueryString(filename, heightInput, widthInput);
+                if (incompleteQuery.length !== 0) {
+                    return [2 /*return*/, res.status(400).send(incompleteQuery.toString())];
+                }
+                height = Number(heightInput);
+                width = Number(widthInput);
+                // check if full image exists
+                if (!fileOperations_1.checkIfFileExists(filename, fullImage)) {
+                    return [2 /*return*/, res.status(404).send(filename + " not found")];
+                }
+                thumbnailFilename = filename.slice(0, filename.lastIndexOf('.'));
+                thumbnailFileExtension = filename.slice(filename.lastIndexOf('.'));
+                targetFilename = thumbnailFilename + "-" + height + "-" + width + thumbnailFileExtension;
+                if (!fileOperations_1.checkIfFileExists(targetFilename, thumbnailImage)) return [3 /*break*/, 1];
+                // return the image thumbnail
+                return [2 /*return*/, res
+                        .status(200)
+                        .sendFile(path.join(__dirname, "../../assets/" + thumbnailImage + "/" + targetFilename))];
+            case 1: return [4 /*yield*/, fileOperations_1.createThumbnailImage(filename, targetFilename, height, width)];
+            case 2:
+                imageResized = _a.sent();
+                if (imageResized === false) {
+                    return [2 /*return*/, res
+                            .status(500)
+                            .send(filename + " thumbnail could not be created.")];
+                }
+                else {
+                    return [2 /*return*/, res
+                            .status(200)
+                            .sendFile(path.join(__dirname, "../../assets/" + thumbnailImage + "/" + targetFilename))];
+                }
+                _a.label = 3;
+            case 3: return [2 /*return*/];
+        }
+    });
+}); });
+exports["default"] = routes;
